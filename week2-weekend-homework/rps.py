ebc3159dd67fcdb467f9719f2a78d3e1d9cc1a61
Joshua Goltry <jgoltry1994@Joshuas-MacBook-Pro.local>
2017-01-30 12:12:49 -0500

    hw

---
+++ week2-weekend-homework/rps.py
@@ -0,0 +1,125 @@
+import random
+import os
+import time
+
+<<<<<<< HEAD
+print("\n")
+print('Rock', 'Paper', 'Scissors')
+print("\n")
+=======
+print("\n***********************************************")
+print("***********  Rock, Paper, Scissors  ***********")
+print("***********************************************\n")
+>>>>>>> 31cca0c12b15231e529d1237a3616ca255179778
+
+
+class Player:
+	def __init__(self, wins=0):
+		self.name = input("Enter your name: ")
+		self.wins = wins
+		self.choice = None
+
+	def make_choice(self):
+		player_choice = input("{}, choose [1] rock, [2] paper or [3] scissors: ".format(self.name))
+		if player_choice not in ['1', '2', '3']:
+			print ('Invalid choice - please choose [1], [2], or [3]')
+			time.sleep(2)
+			self.make_choice()
+		else:
+			self.choice = player_choice
+
+
+class Game:
+	def __init__(self, target_score=3):
+		self.players = []
+		self.target_score = target_score
+		self.winner = None
+		self.round_count = 0
+		self.add_player()
+		self.round()
+
+	def add_player(self):
+		self.players.append(Player())
+		self.add_more_players()
+		#need to validate yes or no
+
+		return self.players
+
+	def add_more_players(self):
+		more_players = input("Will there be more players? [y or n] ")
+		print ("\n")
+		#need to validate yes or no
+		if more_players == "y":
+			self.add_player()
+		elif more_players == 'n':
+			pass
+		else:
+			print('Not a valid input. Please type "y" or "n" on next try.')
+			time.sleep(3)
+			self.add_more_players()
+
+		return self.players
+
+	def round(self):
+		self.round_count += 1
+		os.system('clear')
+<<<<<<< HEAD
+		print("\n")
+		print("Round {}".format(self.round_count))
+		print("\n")
+=======
+		print("\n***********************************************")
+		print("*******************  Round #{} *****************".format(self.round_count))
+		print("***********************************************\n")
+>>>>>>> 31cca0c12b15231e529d1237a3616ca255179778
+
+		for player in self.players:
+			player.make_choice()
+		self.compare()
+		self.score_board()
+		if self.check_wins():
+			self.results()
+		else:
+			self.round()
+
+	def random_computer_choice(self):
+		rps = ['1', '2', '3']
+		comp_choice = random.choice(rps)
+		return comp_choice
+
+	def compare(self):
+		computer_choice = self.random_computer_choice()
+		for player in self.players:
+			if computer_choice == '1' and player.choice == '2':
+				player.wins += 1
+			elif computer_choice == '2' and player.choice == '3':
+				player.wins += 1
+			elif computer_choice == '3' and player.choice == '1':
+				player.wins += 1
+
+	def score_board(self):
+		print ("\n")
+		for player in self.players:
+			print("{} has {} wins".format(player.name, player.wins))
+		time.sleep(3)
+
+	def check_wins(self):
+		for player in self.players:
+			#How to deal with multiple players scoring 5
+			if player.wins == self.target_score:
+				self.winner = player
+				return True
+			else:
+				return False
+
+	def results(self):
+		print ('{} Wins!!'.format(self.winner)) #This should move elsewhere because it doesn't work in round method
+		#create wins display (overall score) after each round , does it go here?
+
+
+g = Game()
+#print (g.rand_comp_choice())
+#guess = Guess()
+#print (guess.player_choice())
+# p = Player(self)
+# p.name
---
+++ week2-weekend-homework/securitize.md
@@ -0,0 +1,32 @@
+'''What is self? Why do all classes and methods need self?''' 
+
+#Self is how you access variables inside a particular class; self also refers to a newly created object and in other class methods. 
+#It refers to the instance whose method is called; also, self has to be the first parameter within a class. 
+#So, whatever 
+#The reason why classes and methods need self is because it is a temporay placeholder for the object
+
+
+
+class Securitization: 
+	def __init__(self, select, package, name):
+		self.select = select
+		self.package = package 
+		self.name = name 
+
+	def MBS_name(self):
+		name = input('What kind of MBS is this?')
+		if type(self.name) is str:
+			print('CDO, but you can also buy a CDS as insurance against default')
+			return True 
+		elif type(self.name) is str:
+			print ('')
+			return True 
+		else: 
+			print('High default risk')
+			return False
+
+
+MBS1 = Securitization('Mortgages', 'DB', 'CDO')
+MBS2 = Securitization('CC debt', 'GS', 'CLO')
+
+Securitization.MBS_name(MBS1
